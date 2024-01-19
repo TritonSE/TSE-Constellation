1756ce721d912174acf12e8fa2f89b7fa7414934
Andrew <Cubified@users.noreply.github.com>
2024-01-19 14:27:40 -0800

    Change development page favicon

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,9 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button className="">abc</Button>
+      <h1>TSE Design System</h1>
+      <p>This page is under construction.</p>
+      <Button className="">Sample Button</Button>
     </>
   )
 }
