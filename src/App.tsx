86bc7fb50f3deef1476e02d2dc0ea2aa90c5f9ad
Philip Zhang <code@philipzha.ng>
2024-02-03 21:17:41 -0800

    preview buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import { Button, Label, Input } from '../';
-import { IconTest } from './IconTest';
-import styles from './styles.css';
+import { ButtonTest } from "./ButtonTest";
+import { IconTest } from "./IconTest";
 
 function App() {
-  return (<>
-    <div style={{
-      width: '100vw',
-      textAlign: 'center',
-      padding: '10rem 0',
-      color: 'var(--color-primary-light)',
-      background: 'var(--color-primary-dark)',
-    }}>
-      <img src='/bulb.png' alt='TSE Logo' width={64} height={64} />
-      <h1>TSE Constellation</h1>
-      <p>A unified design system for Triton Software Engineering.</p>
-    </div>
-    <IconTest />
-  </>);
+  return (
+    <>
+      <div
+        style={{
+          width: "100vw",
+          textAlign: "center",
+          padding: "10rem 0",
+          color: "var(--color-primary-light)",
+          background: "var(--color-primary-dark)",
+        }}
+      >
+        <img src="/bulb.png" alt="TSE Logo" width={64} height={64} />
+        <h1>TSE Constellation</h1>
+        <p>A unified design system for Triton Software Engineering.</p>
+      </div>
+
+      {/* Component Previews */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "20px",
+        }}
+      >
+        <IconTest />
+        <ButtonTest />
+      </div>
+    </>
+  );
 }
 
 export default App;
