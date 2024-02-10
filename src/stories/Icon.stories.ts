496a9a178c97613d053b94af2d5974391c4966c6
Philip Zhang <code@philipzha.ng>
2024-02-10 14:16:54 -0800

    add icon

---
+++ src/stories/Icon.stories.ts
@@ -0,0 +1,75 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Icon } from "../../lib/atoms/Icon";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Atoms/Icon",
+  component: Icon,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    size: { control: "number" },
+    stroke: { control: "color" },
+    fill: { control: "color" },
+  },
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+
+/**
+ * Default icon with no additional props
+ */
+export const Default: Story = {
+  args: {
+    name: "ic_notification",
+  },
+};
+
+/**
+ * Icon with custom size
+ */
+export const Size: Story = {
+  args: {
+    name: "ic_pending",
+    size: 100,
+  },
+};
+
+/**
+ * Icon with custom stroke color
+ */
+export const Stroke: Story = {
+  args: {
+    name: "ic_return",
+    stroke: "red",
+  },
+};
+
+/**
+ * Icon with custom fill color
+ */
+export const Fill: Story = {
+  args: {
+    name: "ic_pending",
+    fill: "green",
+  },
+};
+
+/**
+ * Icon with custom styles
+ */
+export const CustomStyle: Story = {
+  args: {
+    name: "ic_gift",
+    style: { width: 120, height: 120, marginTop: 50 },
+  },
+};
