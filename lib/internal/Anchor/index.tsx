1df9e03c0afcb0284a805cdfcf8a8d178fac5d58
benjaminjohnson2204 <ben332004@gmail.com>
2024-02-18 20:47:19 -0800

    Add documentation for internal Anchor component

--- lib/internal/Anchor/index.tsx
+++ lib/internal/Anchor/index.tsx
@@ -3,13 +3,38 @@ import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 export interface AnchorProps {
+  /**
+   * Whether the anchor is currently open (should be displayed).
+   */
   open: boolean;
+
+  /**
+   * Callback fired when the anchor is closed due to user clicking somewhere else.
+   */
   onClose: () => unknown;
+
+  /**
+   * Element that this component should be anchored to (i.e. have its position track).
+   */
   anchorElement: HTMLElement;
+
+  /**
+   * Placement for anchor; specifies which of the 4 sides of the anchorElement
+   * it should be anchored to, and its alignment along that side.
+   */
   placement: Placement;
+
+  /**
+   * Children to be displayed inside the floating anchor.
+   */
   children: React.ReactNode;
 }
 
+/**
+ * Internal component that anchors its children to float above the DOM near a
+ * certain element. For example, can be used to display a popover below a button,
+ * or a tooltip  above a text field.
+ */
 export function Anchor(props: AnchorProps) {
   const { open, onClose, anchorElement, placement, children } = props;
 
@@ -34,6 +59,7 @@ export function Anchor(props: AnchorProps) {
     }
   };
 
+  // Click listeners to handle user clicking somewhere else, which should close the anchor.
   useEffect(() => {
     document.addEventListener('click', handleDocumentClick, {
       passive: true
@@ -48,7 +74,8 @@ export function Anchor(props: AnchorProps) {
   }, [open, elements, update]);
 
   return open
-    ? createPortal(
+    ? // Use a React portal to render our children at the document root
+      createPortal(
         <div style={floatingStyles} ref={refs.setFloating}>
           {children}
         </div>,
