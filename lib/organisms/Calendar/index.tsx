2475cc601e3c5199d1cb4ea5970aa7e2390dba7f
Andrew <Cubified@users.noreply.github.com>
2024-05-23 11:29:08 -0700

    Move CalendarProps docstrings to upstream

--- lib/organisms/Calendar/index.tsx
+++ lib/organisms/Calendar/index.tsx
@@ -2,28 +2,7 @@ import { Calendar as SimpleCalendar } from "@cubedoodl/react-simple-scheduler";
 
 import "./styles.css";
 
-export type CalendarStyles = {
-  container: React.CSSProperties;
-  head: React.CSSProperties;
-  body: React.CSSProperties;
-};
-
-export type CalendarProps = {
-  /**
-   * The currently-selected date (as a Date object)
-   */
-  selected: Date;
-  /**
-   * Callback to set the selected date
-   */
-  setSelected: (val: Date) => void;
-  /**
-   * The style objects to be passed to the calendar's elements.
-   *
-   * See [here](https://github.com/Cubified/react-simple-scheduler?tab=readme-ov-file#style) for more information.
-   */
-  style?: CalendarStyles;
-};
+import type { CalendarProps } from "@cubedoodl/react-simple-scheduler";
 
 /**
  * A calendar (monthly view) component that allows users to select a date.
