29b7de1e602709926ae68dfc6d9e8c8b4c028c1b
benjaminjohnson2204 <ben332004@gmail.com>
2024-12-11 10:07:40 -0800

    Fix Vite build process to not bundle own version of React

--- vite.config.ts
+++ vite.config.ts
@@ -16,20 +16,20 @@ export default defineConfig({
       entry: resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
     },
-  },
-  rollupOptions: {
-    external: ["react", "react/jsx-runtime"],
-    input: Object.fromEntries(
-      glob
-        .sync("lib/**/*.{ts,tsx}")
-        .map((file) => [
-          relative("lib", file.slice(0, file.length - extname(file).length)),
-          fileURLToPath(new URL(file, import.meta.url)),
-        ]),
-    ),
-    output: {
-      assetFileNames: "assets/[name][extname]",
-      entryFileNames: "[name].js",
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("lib/**/*.{ts,tsx}")
+          .map((file) => [
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
