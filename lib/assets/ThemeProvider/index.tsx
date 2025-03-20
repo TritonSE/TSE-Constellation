c5940b9fb2526e2d7f4f62ee57ad95ef6f9485cb
benjaminjohnson2204 <ben332004@gmail.com>
2025-03-19 19:14:06 -0700

    Fix return type of ThemeProvider

--- lib/assets/ThemeProvider/index.tsx
+++ lib/assets/ThemeProvider/index.tsx
@@ -151,5 +151,5 @@ export function ThemeProvider(props: PropsWithChildren<ThemeProviderProps>) {
     document.body.dataset.tseInitialized = "true";
   }, []);
 
-  return children;
+  return <>{children}</>;
 }
